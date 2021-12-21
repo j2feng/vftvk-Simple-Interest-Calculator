564c6e98f93c4570414bdf2382b73187e80d7a27
j2feng <96500944+j2feng@users.noreply.github.com>
2021-12-21 13:47:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (parseFloat(principal) <= 0){
+        alert("Enter a positive number.")
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var resultString = (`If you deposit <span class='highlight'>${principal}</span>, \n
+    at an interest rate of <span class='highlight'>${rate}%</span> . \n
+    You will receive an amount of <span class='highlight'>${parseFloat(interest)}</span> , \n
+    in the year <span class='highlight'>${year}s</span> `)
+    document.getElementById("result").innerHTML = resultString;
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + ".00%";
+}
